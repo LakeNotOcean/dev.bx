a5362cf2c4d0cd6a136b73016e4fad41f90671eb
Nevskiy Vladislav <VNevskii650@gmail.com>
2021-11-02 14:40:21 +0200

    fix PageLayout generate

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
 <?php
 
-/** @var array $menuItemsRef */
+
 /** @var array $movies */
 require_once "./homework-4/lib/template-functions.php";
 require_once "./homework-4/data/menu.php";
 require_once "./homework-4/data/movies.php";
 
-$menuListLayout = renderTemplate("./homework-4/pages/menuList.php",
-	['menuItemsRef' => $menuItemsRef]);
 
-$searchBarLayout = renderTemplate("./homework-4/pages/searchBar.php");
 
-$moviesListLayout = renderTemplate('./homework-4/pages/moviesList.php',
+$moviesListLayout = renderTemplate('./homework-4/pagesAndParts/moviesList.php',
 	['movies' => $movies]);
 
 
 
-$page=renderLayout([
-	'menuList' => $menuListLayout,
-	'header' => $searchBarLayout,
-	'content' => $moviesListLayout,
-]);
+$result=renderLayout(
+	$moviesListLayout,
+);
 
-echo $page;
+echo $result;
+
 
 
 
